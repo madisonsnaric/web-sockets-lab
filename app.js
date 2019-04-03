cd64d511c979fc8b7af2ae6ee3e9d78f05e2d044
Madison Snaric <snaric@usc.edu>
2019-04-02 20:37:20 -0700

    dimensionalized

--- app.js
+++ app.js
@@ -5,7 +5,9 @@ let wss = new WebSocket.Server({ port: process.env.PORT || 8080 });
 wss.on('connection', (ws) => {
 	ws.on('message', (message) => {
 		console.log(`Received: ${message}`);
-		ws.send(message);  
+		wss.clients.forEach((client) => {
+			client.send(message);
+		})
 	});
 
 	// setTimeout(() => {
